b884414df82ebba0e4667c3f6fb1f7fb5b7d6e09
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-10-31 11:16:52 -0700

    Creates users controller and create, show and new methods

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = ["Invalid Credentials"]
+      redirect_to new_session_url
+    end
+  end
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def show
+    @user = User.find_by(params[:id])
+    render :show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :session_token)
+  end
+end
