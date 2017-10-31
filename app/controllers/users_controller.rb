300b155a57d14ddb508b4cbaba95ef21f76b4b3a
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-10-31 13:49:42 -0700

    Adds form for signing_in through new_session_url and users can now logout if already signed_in through form on application_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      login!(user)
+      login!(@user)
       redirect_to user_url(@user)
     else
       flash.now[:errors] = @user.errors.full_messages
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:id])
     render :show
   end
 
